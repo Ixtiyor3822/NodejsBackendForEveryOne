a9ad4b5358245a8ad57e5a1eab5a30b10a73415e
Ixtiyor3822 <ruslanaymbetov@gmail.com>
2021-08-19 15:37:50 +0500

    Random qoshildi

--- 2Dars/matematik.js
+++ 2Dars/matematik.js
@@ -3,22 +3,32 @@
  */
 
 
-// Ildiz ostiga olish
+// Ildiz ostiga olish.
 let a = 4;
 let natija = Math.sqrt(a)
 console.log("Ildiz Natija : " + natija)
 
-// Darajaga ko`tarish 4 ni 3 - darajisini topish
+// Darajaga ko`tarish 4 ni 3 - darajisini topish.
 a = 4;
 natija = Math.pow(a, 3)
 console.log("Daraja Natija : " + natija)
 
-// Haqiyqiy sonni maximumga yaxlitlash
+// Haqiyqiy sonni maximumga yaxlitlash.
 a = 4.2;
 natija = Math.ceil(a)
 console.log("Maximumga yaxlitlash. Natija : " + natija)
 
-// Haqiyqiy sonni minimumga yaxlitlash
+// Haqiyqiy sonni minimumga yaxlitlash.
 a = 4.2;
 natija = Math.floor(a)
 console.log("Minimumga yaxlitlash. Natija : " + natija)
+
+// Tasodifiy sonni aniqlash.
+// 0 dan 1 gacha bo`lgan tasodifiy sonni aniqlab beradi.
+natija = Math.random()
+console.log("Tasodifiy sonni aniqlash. Natija : " + natija)
+
+// 1 dan 10 gacha bo`lgan tasodifiy sonni aniqlash.
+a = 10;
+natija = Math.ceil(Math.random()*a)
+console.log("0 dan 10 gacha bo`lgan tasodifiy sonni aniqlash. Natija : " + natija)
