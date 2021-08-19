5490e9b78c1748a4d1c86f8c4eb153300dc07c3f
Ixtiyor3822 <ruslanaymbetov@gmail.com>
2021-08-19 15:29:51 +0500

    Matematik amallar

---
+++ 2Dars/matematik.js
@@ -0,0 +1,24 @@
+/**
+ * Matematik amallarni ishlash bo`limi. Ushbu bo`limda Matematik amallarni ishlaymiz.
+ */
+
+
+// Ildiz ostiga olish
+let a = 4;
+let natija = Math.sqrt(a)
+console.log("Ildiz Natija : " + natija)
+
+// Darajaga ko`tarish 4 ni 3 - darajisini topish
+a = 4;
+natija = Math.pow(a, 3)
+console.log("Daraja Natija : " + natija)
+
+// Haqiyqiy sonni maximumga yaxlitlash
+a = 4.2;
+natija = Math.ceil(a)
+console.log("Maximumga yaxlitlash. Natija : " + natija)
+
+// Haqiyqiy sonni minimumga yaxlitlash
+a = 4.2;
+natija = Math.floor(a)
+console.log("Minimumga yaxlitlash. Natija : " + natija)
