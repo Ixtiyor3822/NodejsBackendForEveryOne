ee342d447f7fd1943dd8310b24bfb147ea2b495a
Ixtiyor3822 <ruslanaymbetov@gmail.com>
2021-08-19 11:56:16 +0500

    Ozgaruvchilar hosil qilish.

---
+++ 1Dars/uzgaruvchilar.js
@@ -0,0 +1,17 @@
+/**
+ * Javascripda o`zgaruvchi elon qilishning 3 turi mavjud. let, var, const
+ * let va var o`zgaruvchilar ni elon qilish uchun ishlatsak, const ni o`zgarmaslar uchun ishlatamiz.
+ */
+
+let olma
+let yosh
+const ism
+
+
+/**
+ * O`zgaruvchilarga qiymat berish = belgisidan keyin bo`ladi.
+ * Ma`lumotlar turlariga qarab o`zgaruvchilarga qiymat berila boshlanadi.
+*/
+olma = "Olma mevasi"
+yosh = 23
+ism = "Ixtiyor"
