370cd0494f042c5b0097091f2cd19227e86c8ce2
Ixtiyor3822 <ruslanaymbetov@gmail.com>
2021-08-19 15:07:01 +0500

    Arifmetik amallar

---
+++ 2Dars/arifmetik.js
@@ -0,0 +1,27 @@
+/**
+ * Bu bo`limda arifmetik amallar qilishni ko`rib chiqamiz. 
+ * +,-, /,* amallarini ishlagan holda
+ */
+
+// Eng birinchi bo`lib 2 ta son hosil qilib olamiz.
+// Birinchi son 
+let a = 3;
+// Ikkinchi son
+let b = 2;
+
+// Natijani ko`rish uchun faqat 
+let n
+
+// Amallar ketma ketligini boshlaymiz. 
+// qo`shish amali.
+n = a+b;
+console.log("Yig`indini topish: " + n)
+// Ayirish amali
+n = a - b;
+console.log("Ayirmasini topish : " + n)
+// Ko`paytmasini topish
+n = a * b;
+console.log("Ko`paytmasini topish: " + n)
+// Bo`linmasini topish
+n = a/b;
+console.log("Bo`linmasini topish : " + n)
