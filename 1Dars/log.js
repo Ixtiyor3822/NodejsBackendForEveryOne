5925bf6a03ed0dc8005418c8aa5f785041e46eb1
Ixtiyor3822 <ruslanaymbetov@gmail.com>
2021-08-19 14:23:24 +0500

    console ni ishlatish

---
+++ 1Dars/log.js
@@ -0,0 +1,21 @@
+/**
+ * Biz ishlagan ishlarimizni hato yoki to`g`ri ekanligini ko`rish kerak bo`ladigan yerlari bor. Biz mana shu ishni konsole orqali qilamiz,
+ * console bu bizga ko`makchi vazifasini bajaradi. 
+ * biz asosan log larni ko`rishimiz kk. 
+ */
+
+
+// Matnli ma`lumotlar yuborishingiz mumkin. 
+console.log("Qandaydir matn yuborishingiz mumkin")
+// Number turiga mansub ma`lumotlarini yuborishingiz mumkin.
+console.log(3)
+// Boolean turdagi ma`lumotlarni yuborishingiz mumkin. 
+console.log(true)
+console.log(false)
+// Obekt larni ham yuborishingiz mumkin.
+console.log({
+    name: "Ixtiyor",
+    age: 23
+})
+// Massivlarni ham yuborishingiz mumkin. 
+console.log(["Birinchi", 2, "Salom", "Qani kelgin endi "])
