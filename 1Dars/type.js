5451bd60397eaf53f4dfd48ddbebc38d13daaa2c
Ixtiyor3822 <ruslanaymbetov@gmail.com>
2021-08-19 14:17:17 +0500

    Malumotlar turlari.

---
+++ 1Dars/type.js
@@ -0,0 +1,27 @@
+/**
+ * Malumotlar turlari: Number, String, Boolean
+ */
+
+// Number -> ma`lumot turiga misollar:
+// Butun sonlar
+let a = 1;
+//  Manfiy sonlar.
+let son = -23;
+// Haqiyqiy sonlar.
+let haqiyqiy = 32.43;
+
+
+// String  turdagi ma`lumotlar.
+
+// harflar
+let m = `a`;
+// matnlar
+let matn = 'Assalomu aleykum.';
+// bitta so`z
+let soz = "Sozlar";
+
+// Boolean turdagi ma`lumotlar.
+// ROst
+var rost = true;
+// Yolg`on
+var yolgon = false;
